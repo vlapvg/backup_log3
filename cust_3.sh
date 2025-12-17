e20c1b0b5773e5ea1e8f7488a79f8b695b760ea1
Vladimir Paltsev <vlap-d@yandex.ru>
2025-12-17 21:09:59 +0500

    Update cust_3.sh

--- cust_3.sh
+++ cust_3.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-cp ports.conf /etc/apache2; cp 000-default.conf /etc/apache2/sites-enabled; 
+systemctl stop apache2; systemctl start nginx; 
+\cp ports.conf /etc/apache2; cp 000-default.conf /etc/apache2/sites-enabled; 
 \cp index.html /var/www/html; systemctl start apache2; 
 \systemctl restart prometheus-node-exporter; 
 \cp jvm.options /etc/elasticsearch/jvm.options.d; cp elasticsearch.yml /etc/elasticsearch; 
@@ -8,4 +9,4 @@ cp ports.conf /etc/apache2; cp 000-default.conf /etc/apache2/sites-enabled;
 \cp logstash.yml /etc/logstash; cp logstash-nginx-es.conf /etc/logstash/conf.d; systemctl restart logstash.service; 
 \cp filebeat.yml /etc/filebeat; systemctl restart filebeat; 
 \cp mysqld.cnf /etc/mysql/mysql.conf.d; service mysql restart; 
-\cp mysqldamp.sh /usr/local/bin; cp root /var/spool/cron/crontabs; systemctl restart cron; ./script_replica.sh
+\cp mysqldamp.sh /usr/local/bin; cp root /var/spool/cron/crontabs; ./script_replica.sh
